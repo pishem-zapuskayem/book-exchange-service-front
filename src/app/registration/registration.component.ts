34697b0af3d21498828fa5d771fa1e860cf25d6a
Frozent <Frozent.03@mail.ru>
2024-03-22 22:56:11 +0400

    попытка валидации

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators, AbstractControl} from "@angular/forms";
 import {RegisterDTO} from "../core/interfaces/register.dto";
 import {Observer} from "rxjs";
 import {HttpResponse} from "@angular/common/http";
@@ -12,75 +12,82 @@ import {fetchPackageManifest} from "@angular/cli/src/utilities/package-metadata"
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
-  form: FormGroup;
+  form: FormGroup = new FormGroup({
+    lastName: new FormControl(''),
+    firstName: new FormControl(''),
+    secondName: new FormControl(''),
+    email: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+    addrIndex: new FormControl(''),
+    addrCity: new FormControl(''),
+    addrStreet: new FormControl(''),
+    addrHouse: new FormControl(''),
+    addrStructure: new FormControl(''),
+    addrApart: new FormControl(''),
+  });
   isSubmitted = false;
   error = false;
   selectedFile: File | null = null;
-
   constructor(
+    private formBuilder: FormBuilder,
     private router: Router,
-    private service: AuthService
-  ) {
-
-    this.form = new FormGroup({
-
-      lastName: new FormControl(null, [
+    private service: AuthService,
+  ) {}
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+      lastName: ["" ,[ Validators.required, Validators.maxLength(50), Validators.pattern(/[А-Я]/)]],
+      firstName:["", [Validators.required, Validators.maxLength(25), Validators.pattern(/[А-Я]/) ]],
+      secondName:["", [Validators.required, Validators.maxLength(25), Validators.pattern(/[А-Я]/) ]],
+      email: [null, [
         Validators.required,
-        Validators.maxLength(50)
-      ]),
-      firstName: new FormControl(null, [
+        Validators.maxLength(50),
+        Validators.email
+      ]],
+      username: [null, [
         Validators.required,
-        Validators.maxLength(25)
-      ]),
-      secondName: new FormControl(null, [
+        Validators.minLength(2),
+        Validators.pattern(/([$@$!%*?&])/)
+      ]],
+      password: [null, [
         Validators.required,
-        Validators.maxLength(25)
-      ]),
-      email: new FormControl(null, [
-        Validators.required,
-        Validators.maxLength(50)
-      ]),
-      username: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(2)
-      ]),
-      password: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(8)
-      ]),
-      addrIndex: new FormControl(null, [
+        Validators.minLength(8),
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/)
+      ]],
+      addrIndex: [null, [
         Validators.required,
         Validators.maxLength(7)
-      ]),
-      addrCity: new FormControl(null, [
+      ]],
+      addrCity: [null, [
         Validators.required,
         Validators.maxLength(15)
-      ]),
-      addrStreet: new FormControl(null, [
+      ]],
+      addrStreet: [null, [
         Validators.required,
         Validators.maxLength(25)
-      ]),
-      addrHouse: new FormControl(null, [
+      ]],
+      addrHouse: [null, [
         Validators.required,
         Validators.maxLength(5)
-      ]),
-      addrStructure: new FormControl(null, [
+      ]],
+      addrStructure: [null, [
         Validators.required,
         Validators.maxLength(2)
-      ]),
-      addrApart: new FormControl(null, [
+      ]],
+      addrApart: [null, [
         Validators.required,
         Validators.maxLength(3)
-      ]),
+      ]],
       avatar: new FormControl(null, []),
     });
-
   }
-
   goToHomePage() {
     this.router.navigate(['/']);
   }
-
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
   submit() {
     if (this.isSubmitted || this.form.invalid) {
       return;
@@ -120,3 +127,7 @@ export class RegistrationComponent {
     this.selectedFile = input.files[0] as File;
   }
 }
+
+
+
+
