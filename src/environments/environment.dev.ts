e58fde903c013007f7a56f356b4c124e12a5610a
vl7sha <vl7shab@yandex.ru>
2024-03-29 22:22:56 +0400

    add proxy config

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,10 +1,10 @@
 export const environment = {
-  SIGN_IN_URL: 'http://82.97.243.70:8085/api/v1/auth/sign-in',
-  SIGN_UP_URL:'http://82.97.243.70:8085/api/v1/sign-up',
+  SIGN_IN_URL: 'http://localhost:4200/api/v1/auth/sign-in',
+  SIGN_UP_URL:'http://localhost:4200/api/v1/sign-up',
   TOKEN_KEY: 'jwt',
   EXP_DATE_KEY: 'expiresIn',
   CLIENT_DOMAIN: 'http://localhost:4200',
-  BACKEND_DOMAIN: 'http://82.97.243.70:8085',
+  BACKEND_DOMAIN: 'http://localhost:4200',
   ROLE_ADMIN: 'ROLE_ADMIN',
   ROLE_ANONYMOUS: 'ROLE_ANONYMOUS',
 }
