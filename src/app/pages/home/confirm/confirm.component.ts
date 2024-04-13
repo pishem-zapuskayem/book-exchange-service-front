ae01f981e82b743d223b8220c362df42089d9b7c
Frozent <Frozent.03@mail.ru>
2024-04-13 22:34:43 +0400

    Если почта подтверждена, перекидывает на главную и открывает окно с логином

--- src/app/pages/home/confirm/confirm.component.ts
+++ src/app/pages/home/confirm/confirm.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {AuthService} from "../../../core/services/auth.service";
+import {AuthDialogComponent} from "../../../auth-dialog/auth-dialog.component";
+import {MatDialog} from "@angular/material/dialog";
 
 
 
@@ -15,6 +17,8 @@ export class ConfirmComponent {
   constructor(
     private route: ActivatedRoute,
     private authService: AuthService,
+    private router: Router,
+    public dialog: MatDialog,
   ) {
   }
   ngOnInit(): void {
@@ -24,6 +28,8 @@ export class ConfirmComponent {
         this.authService.confirmRegistration(data)
           .subscribe(res => {
             this.message='Почта подтверждена'
+            this.router.navigate(['/home'])
+            this.dialog.open(AuthDialogComponent);
           }, err => {
             if (err) {
               this.message='Не удалось подтвердить почту'
